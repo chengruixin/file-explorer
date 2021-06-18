3661357098dd6a556404a0fe77f2dadf8cd5ab87
chengruixin <chengruixin_2010@163.com>
2021-06-19 00:49:08 +0800

    introduce raxSearch lib

---
+++ src/test.js
@@ -0,0 +1,12 @@
+const {findExactItems} = require("./../libs/raxSearch/raxSearch.dev").exactMatcher;
+
+
+const a = findExactItems("abc")
+
+console.log(a)
+
+const str = "asdfasd";
+
+const res = a.findFirst(str)
+
+console.log(res)
