debaea4cd22f086f819eb0352672b4d05400b62c
chengruixin <chengruixin_2010@163.com>
2021-08-30 05:18:42 +0800

    throttle the forward

--- client/src/components/VideoDisplay/Player.js
+++ client/src/components/VideoDisplay/Player.js
@@ -7,14 +7,21 @@ const videoProgressChange = 3
 export default function Player({ url }) {
     const classes = useStyles()
     const videoRef = useRef()
-
+    let timer
     const handleEvents = (e) => {
         e.preventDefault()
         const { keyCode } = e
         if (keyCode === 37) {
             videoRef.current.currentTime -= videoProgressChange
         } else if (keyCode === 39) {
-            videoRef.current.currentTime += videoProgressChange
+            if (!timer) {
+                videoRef.current.playbackRate = 8
+                console.log(1);
+                timer = setTimeout(() => {
+                    videoRef.current.playbackRate = 1
+                    timer = null
+                }, 500)
+            }
         } else if (keyCode === 38) {
             const currentVolume = videoRef.current.volume
 
