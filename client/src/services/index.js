893ea6b6c66d8f8c71babfaaf90b319a3f818ba0
chengruixin <chengruixin_2010@163.com>
2022-04-11 01:21:09 +0800

    prepare upgrade to react 18

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 export const fetchVideos = async (val) => {
     try {
-        const res = await axios.get('/query?search=' + val)
+        const res = await axios.get('/videos?search=' + val)
+        console.log(res)
         return res
     } catch (err) {
         throw err
