e070a24956c6de6fac94d9e82688feecc49a4a78
chengruixin <chengruixin_2010@163.com>
2022-05-18 23:42:05 +0800

    update shingler

--- goserver/cmd/test.go
+++ goserver/cmd/test.go
@@ -6,38 +6,9 @@ import (
 )
 
 func main() {
-	strs := []string{
-		"I am a text with some texts and I dont know what to talk",
-		"Here is another one text ",
-		"Another one, but short",
-		"Remind that labels and other not english letters symbols are not counted in this algo",
-		"Remind that only english letters will be labels and other counted in this algo",
-		"SPLU-zxc",
-		"SPLU-sef",
-		"SPLU-xdf",
-		"SPLU-abc",
-		"SPLU-adb",
-	}
+	str := "abcde efg, hkl"
 
-	tested := []string{
-		"Remind that only english letters symbol are not counted",
-		"another one",
-		"continuing",
-		"splu",
-	}
-
-	worker1 := similarity.NewLSHWorker(200, 2, 2)
-	worker2 := similarity.NewLSHWorker(200, 2, 2)
-	worker3 := similarity.NewLSHWorker(200, 2, 2)
-
-	worker1.MultiAddText(strs)
-	worker2.MultiAddText(strs)
-	worker3.MultiAddText(strs)
-
-	for _, t := range tested {
-		res := append(worker1.FindCandidates(t), append(worker2.FindCandidates(t), worker3.FindCandidates(t)...)...)
-		fmt.Println(similarity.RemoveRepeated(res))
-	}
+	fmt.Println(similarity.NewShingleExecutor(str, 3).ShingleValueComplex())
 }
 
 // func getSigs(str string, hashFuncs []*similarity.HashFuncProps) []int {
