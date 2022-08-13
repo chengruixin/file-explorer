37455f154ca3b0aef38598e6629bdb60fddfa52b
chengruixin <chengruixin_2010@163.com>
2022-08-13 20:51:30 +0800

    feat: remove extra react app

--- rax_web/src/index.tsx
+++
@@ -1,6 +0,0 @@
-import * as ReactDOM from "react-dom/client";
-import App from "./app";
-
-const container = document.getElementById("root") as Element;
-const root = ReactDOM.createRoot(container);
-root.render(<App />);
